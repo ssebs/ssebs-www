7a6225c25721dea4cc6c8a3ae025a90c5ea25fd5
Sebastian Safari <ssebs@ymail.com>
2019-07-30 20:18:57 -0700

    fix colors

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,20 @@ const About = () => {
     let classes = "text-center header";
     if (window.location.hash === "#/") {
         console.log(window.location.hash);
-        classes = "text-center header text-tertiary";
+        classes = "text-center header text-white";
     }
     return (
         <div>
             <h1 className={classes}>ABOUT</h1>
             <p>
-               <span className="text-tertiary" style={{fontSize:"1.75rem"}}>ssebs</span> is my username for most things on the web. My real name is <em className="text-tertiary">Sebastian Safari</em>, and I am a Systems and Software Engineer. What does that mean? I do both System Engineering/Administration, and Software Development. 
+                My name is <strong>Sebastian Safari</strong>, and I am a{" "}
+                <em>Systems and Software Engineer</em>, meaning I do both System
+                Administration, and Software Engineering instead of just one or
+                the other. My username is{" "}
+                <span className="text-tertiary" style={{ fontSize: "1.5rem" }}>
+                    ssebs
+                </span>
+                , you can find me online with that.
             </p>
             <p>
                Day to day I build, develop, automate, and maintain misc IT infrastructure systems and web applications. Some examples of this would be things like:
