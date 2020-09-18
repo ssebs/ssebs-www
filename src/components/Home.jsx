1fb1cc914096930d2e0b66944ff12fe7e65c5525
Sebastian Safari <ssebs@ymail.com>
2020-09-17 17:31:35 -0700

    card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Card from "./WebComponents/Card";
 
+import { ReactComponent as CodeSquare } from "bootstrap-icons/icons/code-square.svg";
+
 const Home = () => {
   return (
     <div className="container">
       <p>ssebs.com site</p>
       <button className="btn btn-secondary">Test Button</button>
-      <Card title="Proj1" icon="graph">
+
+      <Card title="Proj1" icon={<CodeSquare/>}>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque maxime
         placeat ducimus similique.
       </Card>
--- src/components/WebComponents/Card.jsx
+++ src/components/WebComponents/Card.jsx
@@ -3,10 +3,16 @@ import React from "react";
 const Card = (props) => {
   const { title, icon } = props;
   return (
-    <div className="card">
-      <div className="card-header">{title}</div>
-      <div className="card-body">{icon}</div>
-      <div className="card-text">{props.children}</div>
+    <div className="card bg-light">
+      <div className="card-body">
+        <div className="card-title text-center">
+          <strong>{title}</strong>
+          <div className="d-flex justify-content-center mt-3 icon text-tertiary">
+            {icon}
+          </div>
+        </div>
+      </div>
+      <div className="card-footer text-white bg-primary">{props.children}</div>
     </div>
   );
 };
