9d51f9bb8ab50d77657a1109e3ae9bd5c816063d
Sebastian Safari <ssebs@ymail.com>
2024-07-07 15:14:45 -0700

    env tweak

--- setup_env.sh
+++ setup_env.sh
@@ -2,12 +2,18 @@
 # This script is used to setup your dev environment. Basically just installs required pkgs, currently using this in code-server docker container.
 sudo apt update
 sudo apt upgrade -y
-sudo apt install hugo golang -y
+sudo apt install golang -y
+
+mkdir tmp
+cd tmp
+# HACK - gotta use the latest release. 
+curl -LJO https://github.com/gohugoio/hugo/releases/download/v0.128.2/hugo_extended_0.128.2_linux-amd64.tar.gz
+tar xf ./hugo_extended_0.128.2_linux-amd64.tar.gz
 
 echo 
 echo "Install php twig and configure the template files in VSCode to use twig lang for formatting!"
 echo "https://stackoverflow.com/a/60644216"
 echo 
 echo "To run server:"
-echo "    hugo serve --noHTTPCache --disableFastRender --bind 0.0.0.0"
+echo "    ./tmp/hugo serve --noHTTPCache --disableFastRender --bind 0.0.0.0"
 echo
