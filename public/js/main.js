60073a9e740b8f45d4e10af5438d78bfce0a19a1
Sebastian Safari <ssebs@ymail.com>
2024-07-16 22:57:38 -0700

    move script

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,24 @@
 const myAlert = (msg) => {
     window.alert(msg)
 }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    // Main menu toggle
+    var toggleOpen = document.getElementById('toggleOpen');
+    var collapseMenu = document.getElementById('collapseMenu');
+
+    toggleOpen.addEventListener("click", () => {
+        toggleElement(collapseMenu);
+    })
+
+    function toggleElement(element) {
+        if (element.classList.contains("showit")) {
+            element.classList.remove("showit")
+            element.classList.add("hideit");
+        } else {
+            element.classList.remove("hideit")
+            element.classList.add("showit");
+        }
+    }
+    
+})
