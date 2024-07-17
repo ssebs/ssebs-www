a34e1303e5e3f99c9633a2b52ab9828223765000
Sebastian Safari <ssebs@ymail.com>
2024-07-16 22:59:50 -0700

    put it back

--- static/js/main.js
+++ static/js/main.js
@@ -3,23 +3,6 @@ const myAlert = (msg) => {
     window.alert(msg)
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    // Main menu toggle
-    var toggleOpen = document.getElementById('toggleOpen');
-    var collapseMenu = document.getElementById('collapseMenu');
+// document.addEventListener("DOMContentLoaded", function (event) {
 
-    toggleOpen.addEventListener("click", () => {
-        toggleElement(collapseMenu);
-    })
-
-    function toggleElement(element) {
-        if (element.classList.contains("showit")) {
-            element.classList.remove("showit")
-            element.classList.add("hideit");
-        } else {
-            element.classList.remove("hideit")
-            element.classList.add("showit");
-        }
-    }
-    
-})
+// })
