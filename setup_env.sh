99d17eecf66105d006e2d801f8dabacc2d6c3789
Sebastian Safari <ssebs@ymail.com>
2024-07-22 18:26:32 -0700

    env

--- setup_env.sh
+++ setup_env.sh
@@ -7,8 +7,18 @@ sudo apt install golang -y
 mkdir tmp
 cd tmp
 # HACK - gotta use the latest release. 
-curl -LJO https://github.com/gohugoio/hugo/releases/download/v0.128.2/hugo_extended_0.128.2_linux-amd64.tar.gz
-tar xf ./hugo_extended_0.128.2_linux-amd64.tar.gz
+if [ ! -f ./hugo_extended_0.128.2_linux-amd64.tar.gz ]; then
+    curl -LJO https://github.com/gohugoio/hugo/releases/download/v0.128.2/hugo_extended_0.128.2_linux-amd64.tar.gz
+    tar xf ./hugo_extended_0.128.2_linux-amd64.tar.gz
+fi
+
+# npm
+curl -fsSL https://fnm.vercel.app/install | bash
+source ~/.bashrc
+fnm use --install-if-missing 22
+cd ../themes/ssebs/
+npm install
+npm run build
 
 echo "To run server:"
 echo "./tmp/hugo serve --noHTTPCache --disableFastRender --bind 0.0.0.0"
